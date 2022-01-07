774c9b2ea3a3915605d50542eb321860b7a83811
Sérgio Fleury Dias Filho <sergiofdf@gmail.com>
2022-01-07 13:59:38 -0300

    add transitions

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Switch } from  'react-router-dom';
+import { Route, Switch, useLocation } from  'react-router-dom';
+import { useTransition, animated } from 'react-spring'
 
 import Home from './pages/Home';
 import Posts from './pages/Posts';
@@ -7,12 +8,24 @@ import NotFound from './pages/NotFound';
 import Post from './pages/Post';
 
 export default function Routes(){
-  return(
-    <Switch>
-     <Route exact path="/" component={Home}/>
-     <Route exact path="/posts" component={Posts}/>
-     <Route path="/posts/:id" component={Post}/>
-     <Route component={NotFound} />
-    </Switch>
+
+  const location = useLocation();
+  const transitions = useTransition(location, {
+    from: { opacitiy: 0, transform: 'translateY(50px)' },
+    enter: { opacitiy: 1, transform: 'translateY(0px)' },
+    leave: { display:'none', position: 'absolute' },
+  });
+  
+  return( transitions((props, item) => (
+    <animated.div style={props}>
+      <Switch location={item}>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/posts" component={Posts}/>
+        <Route path="/posts/:id" component={Post}/>
+        <Route component={NotFound} />
+      </Switch>
+    </animated.div>
+  ))
+    
   );
 }
